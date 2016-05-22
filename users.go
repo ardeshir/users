0f1034108b4f1f5d0865108b8cfe868cf6247d8b
Ardeshir Sepahsalar <asepahsalar@hennepintech.edu>
2016-05-22 21:34:14 +0000

    adding w.Writer

--- users.go
+++ users.go
@@ -1,6 +1,6 @@
 package users
 
-import(
+import (
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -11,6 +11,21 @@ import(
 type User struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
-	Email	  string `json:"email"`
+	Email     string `json:"email"`
 }
 
+var userStore = []User{}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := json.Marshal(userStore)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(users)
+
+}
